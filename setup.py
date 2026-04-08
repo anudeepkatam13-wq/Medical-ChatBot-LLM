dcd785dd3234f0613827746972a38a42d6561226
Anudeep Katam <anudeepkatam13@gmail.com>
2026-04-08 12:25:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages, setup
 setup(
     name="medical_chatbot",
     version="0.1.0",
-    author="Boktiar Ahmed Bappy",
-    author_email="entbappy73@gmail.com",
+    author="Anudeep katam",
+    author_email="anudeepkatam13@gmail.com",
     packages=find_packages(),
     install_requires=[]
 )
